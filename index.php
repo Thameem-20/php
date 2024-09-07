1f9f83b87edaf3788a76f4dd681ba4c162136826
Thameem-20 <ahmedthameem20@gmail.com>
2024-09-07 17:50:09 +0530

    updated

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title></title>
-</head>
-<body>
-    <?php
-        echo "hello world!"
-    ?>
-</body>
-</html>~
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    $con = mysqli_connect($server, $username, $password);
+
+    if (!$con){
+        die("connection failed due to" . mysqli_connect_error());
+    }
+    // echo "success connecting to the db";
+
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $phone = $_POST['phone'];
+    $desc = $_POST['desc'];
+    $sql = "INSERT INTO `trip` ( `name`, `age`, `phone`, `desc`, `dt`) VALUES ('$name', '$age', '$phone', '$desc', current_timestamp());";
+    echo $sql;
+
+    if($con->query($sql) == true){
+        echo "inserted successfully";
+    }
+    else{
+        echo "Error: $sql <br> $con->error";
+    }
+    $con->close();
+?>
